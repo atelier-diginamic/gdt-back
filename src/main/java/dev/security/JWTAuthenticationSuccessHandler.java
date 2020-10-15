8c39d2e342b77b44de352b79e66f638aa5537e56
sylvain amianto <samianto@gmail.com>
2020-10-15 14:13:18 +0200

    edit jws response cookie pour prod

--- src/main/java/dev/security/JWTAuthenticationSuccessHandler.java
+++ src/main/java/dev/security/JWTAuthenticationSuccessHandler.java
@@ -11,6 +11,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
@@ -32,56 +34,58 @@ import java.util.stream.Collectors;
 @Configuration
 public class JWTAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
-    private static final Logger LOG = LoggerFactory.getLogger(JWTAuthenticationSuccessHandler.class);
+	private static final Logger LOG = LoggerFactory.getLogger(JWTAuthenticationSuccessHandler.class);
 
-    @Value("${jwt.expires_in}")
-    private Integer EXPIRES_IN;
+	@Value("${jwt.expires_in}")
+	private Integer EXPIRES_IN;
 
-    @Value("${jwt.cookie}")
-    private String TOKEN_COOKIE;
+	@Value("${jwt.cookie}")
+	private String TOKEN_COOKIE;
 
-    @Value("${jwt.secret}")
-    private String SECRET;
+	@Value("${jwt.secret}")
+	private String SECRET;
 
-    @Autowired
-    private CollegueRepo collegueRepo;
+	@Autowired
+	private CollegueRepo collegueRepo;
 
-    @Autowired
-    private ObjectMapper mapper;
+	@Autowired
+	private ObjectMapper mapper;
 
+	@Override
+	@Transactional
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws IOException, ServletException {
 
-    @Override
-    @Transactional
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-                                        Authentication authentication) throws IOException, ServletException {
+		LOG.info("Génération du token JWT");
 
-        LOG.info("Génération du token JWT");
+		User user = (User) authentication.getPrincipal();
 
-        User user = (User) authentication.getPrincipal();
+		String rolesList = user.getAuthorities().stream().map(a -> a.getAuthority()).collect(Collectors.joining(","));
 
-        String rolesList = user.getAuthorities().stream().map(a -> a.getAuthority()).collect(Collectors.joining(","));
+		Collegue collegue = collegueRepo.findByEmail(user.getUsername())
+				.orElseThrow(() -> new IllegalArgumentException("L'email ne correspond à aucun collegue"));
 
-        Collegue collegue = collegueRepo.findByEmail(user.getUsername()).orElseThrow(() -> new IllegalArgumentException("L'email ne correspond à aucun collegue"));
+		response.setContentType("application/json");
+		response.getWriter().write(mapper.writeValueAsString(new CollegueDtoRep(collegue)));
 
-        response.setContentType("application/json");
-        response.getWriter().write(mapper.writeValueAsString(new CollegueDtoRep(collegue)));
+		Map<String, Object> infosSupplementaireToken = new HashMap<>();
+		infosSupplementaireToken.put("roles", rolesList);
+		LOG.info("roles {} ", infosSupplementaireToken);
 
-        Map<String, Object> infosSupplementaireToken = new HashMap<>();
-        infosSupplementaireToken.put("roles", rolesList);
-        LOG.info("roles {} ", infosSupplementaireToken);
+		String jws = Jwts.builder().setSubject(user.getUsername()).addClaims(infosSupplementaireToken)
+				.setExpiration(new Date(System.currentTimeMillis() + EXPIRES_IN * 1000))
+				.signWith(io.jsonwebtoken.SignatureAlgorithm.HS512, SECRET).compact();
 
-        String jws = Jwts.builder()
-                .setSubject(user.getUsername())
-                .addClaims(infosSupplementaireToken)
-                .setExpiration(new Date(System.currentTimeMillis() + EXPIRES_IN * 1000))
-                .signWith(io.jsonwebtoken.SignatureAlgorithm.HS512, SECRET)
-                .compact();
+		ResponseCookie responseCookie = ResponseCookie.from(TOKEN_COOKIE, jws).httpOnly(true).maxAge(EXPIRES_IN * 1000)
+				.path("/").sameSite("Lax").build();
+		response.setHeader(HttpHeaders.SET_COOKIE, responseCookie.toString());
+		LOG.info("Token JWT généré posé dans un cookie et en entête HTTP");
 
-        Cookie authCookie = new Cookie(TOKEN_COOKIE, (jws));
-        authCookie.setHttpOnly(true);
-        authCookie.setMaxAge(EXPIRES_IN * 1000);
-        authCookie.setPath("/");
-        response.addCookie(authCookie);
-        LOG.info("Token JWT généré posé dans un cookie et en entête HTTP");
-    }
+//        Cookie authCookie = new Cookie(TOKEN_COOKIE, (jws));
+//        authCookie.setHttpOnly(true);
+//        authCookie.setMaxAge(EXPIRES_IN * 1000);
+//        authCookie.setPath("/");
+//        response.addCookie(authCookie);
+//        LOG.info("Token JWT généré posé dans un cookie et en entête HTTP");
+	}
 }

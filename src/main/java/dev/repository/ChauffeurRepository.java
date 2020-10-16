183797166544068df83d524afaad70f8f4da24c2
sylvain amianto <samianto@gmail.com>
2020-10-16 08:47:57 +0200

    correction jpql

--- src/main/java/dev/repository/ChauffeurRepository.java
+++ src/main/java/dev/repository/ChauffeurRepository.java
@@ -3,13 +3,17 @@ package dev.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import dev.entity.Chauffeur;
 
 public interface ChauffeurRepository extends JpaRepository<Chauffeur, Integer>{
 
 	List<Chauffeur> findByMatricule(String value);
+	
+	@Query("select ch from Chauffeur ch where ch.info.nom=?1")
 	List<Chauffeur> findByNom(String value);
+	@Query("select ch from Chauffeur ch where ch.info.prenom=?1")
 	List<Chauffeur> findByPrenom(String value);
 
 }
